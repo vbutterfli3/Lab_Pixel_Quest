62469eac977d80be6446ccd6914dc69a27215cd4
vbutterfli3 <nessag0118@gmail.com>
2025-07-10 12:16:57 -0400

    7/10/25 Walking + Coding

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
-    string variableone = "Good ";
-    int var1 = 3;
+    private Rigidbody2D rb;
+    public int speed = 4;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
-        string variabletwo = "Morning";
-        Debug.Log(variableone + variabletwo);
+        rb = GetComponent<Rigidbody2D>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var1);
-        var1++;
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+
+    /*
+     {
+
+   Debug.Log(xInput);
+
+
+    if (Input.GetKeyDown(KeyCode.W))
+        transform.position += new Vector3(0, 1, 0);
+    if (Input.GetKeyDown(KeyCode.S))
+        transform.position += new Vector3(0, -1, 0);
+    if (Input.GetKeyDown(KeyCode.D))
+        transform.position += new Vector3(1, 0, 0);
+    if (Input.GetKeyDown(KeyCode.A))
+        transform.position += new Vector3(-1, 0, 0);
+        } 
+       */
 }
+
+
