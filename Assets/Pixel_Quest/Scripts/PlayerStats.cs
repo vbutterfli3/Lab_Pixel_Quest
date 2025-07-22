56b6a7362e6ad4f2e82f30df8d7d1db6dc9227c7
vbutterfli3 <nessag0118@gmail.com>
2025-07-22 12:20:46 -0400

    7/22/25 Coins, Hearts, Checkpoints

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -7,6 +7,9 @@ public class PlayerStats : MonoBehaviour
 
 {
     public string nextLevel = "Level2";
+    private int CoinCounter = 0;
+    private int Health = 0;
+    public Transform RespawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -22,16 +25,43 @@ public class PlayerStats : MonoBehaviour
         {
             case "Death":
                 {
-                    string thislevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thislevel);
+                    Health--;
+                    if (Health <= 0)
+                    {
+                        string thislevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thislevel);
+                    }
+                    else { transform.position = RespawnPoint.position; }
                     break;
                 }
+
             case "Finish":
                 {
                     SceneManager.LoadScene(nextLevel);
                     break;
                 }
 
+            case "Coin":
+                {
+                    CoinCounter++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+
+            case "Health":
+                {
+                    if (Health < 3)
+                    {
+                        Health++;
+                        Destroy(collision.gameObject);
+                    }
+                    break;
+                }
+            case "Respawn":
+                {
+                    RespawnPoint.position = collision.transform.Find("Point").position;
+                    break;
+                }
 
         }
 
